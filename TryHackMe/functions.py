d0937499190afab15392948d497f101e7b35ae24
georgegathu <ggeorge.gathu@gmail.com>
2023-05-24 10:52:55 +0300

    Try Hack Me - Python Intro (Functions Example Update)

--- TryHackMe/functions.py
+++ TryHackMe/functions.py
@@ -7,3 +7,20 @@ def sayHello(name):
      print("Hello " + name + "! Nice to meet you.")
 
 sayHello("ben") # Output is: Hello Ben! Nice to meet you
+
+"""
+ Similar to if statements, anything after the colons that is indented is considered part of the function.
+
+A function can also return a result, see the code block below:
+"""
+def calcCost(item):
+     if(item == "sweets"):
+          return 3.99
+     elif (item == "oranges"):
+          return 1.99
+     else:
+          return 0.99
+
+spent = 10
+spent = spent + calcCost("sweets")
+print("You have spent:" + str(spent))
