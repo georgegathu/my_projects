d48ea404b7a333ad8fc1f75893bbbdf6b07e90a1
georgegathu <ggeorge.gathu@gmail.com>
2023-05-24 10:21:51 +0300

    Try Hack Me - Python Intro (For Loop example)

---
+++ TryHackMe/for_loop.py
@@ -0,0 +1,8 @@
+"""
+For Loops
+
+A for loop is used to iterate over a sequence such as a list. Lists are used to store multiple items in a single variable, and are created using square brackets (see below). Let's learn through the following example:
+"""
+websites = ["facebook.com", "google.com", "amazon.com"]
+for site in websites:
+     print(site)
