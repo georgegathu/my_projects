290df54dde0566d5000d7f63a135f07d648ad3b7
georgegathu <ggeorge.gathu@gmail.com>
2023-05-24 10:41:00 +0300

    Try Hack Me - Python Intro (Functions Example)

---
+++ TryHackMe/functions.py
@@ -0,0 +1,9 @@
+"""
+As programs start to get bigger and more complex, some of your code will be repetitive, writing the same code to do the same calculations, and this is where functions come in. A function is a block of code that can be called at different places in your program.
+
+You could have a function to work out a calculation such as the distance between two points on a map or output formatted text based on certain conditions. Having functions removes repetitive code, as the function's purpose can be used multiple times throughout a program.
+"""
+def sayHello(name):
+     print("Hello " + name + "! Nice to meet you.")
+
+sayHello("ben") # Output is: Hello Ben! Nice to meet you
